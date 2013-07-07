8013989d8cb2077ebd45ce9b027635cac6163365
Chris Evdemon <cevdemon@gmail.com>
2013-07-07 21:54:58 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mypin::Application.config.secret_token = '9349e0e87004de4ad149b2ade87758282fa20d94b55c5cb87733c78b8fb30ca70ae12ba7c65da09fece707cad9fd00d2e4be6308842ef96464f037cbdf012055'
