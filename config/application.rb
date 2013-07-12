49dd5ff4cd7b09392f0170f9145b4b1f85d905fb
Chris Evdemon <cevdemon@gmail.com>
2013-07-13 00:21:00 +0800

    Added Devise.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Mypin
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Requested by Devise to run on Heroku.
+    config.assets.initialize_on_precompile = false
+
   end
 end
